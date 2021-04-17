a48a6cc95d9d52c390fb8a96ed307c695fa5760d
Nick Rivadeneira <nickrivadeneira039@gmail>
2021-04-17 11:11:57 -0400

    changes :x

--- assets/scripts.js
+++ assets/scripts.js
@@ -34,8 +34,31 @@ $("#select-bar").on("change", function(event){
     }
 })
 
+document.getElementById("fname-submit").addEventListener("click", checkFname)
 
 
 
 
+function checkFname(event) {
+    var input = document.getElementById("fname")
+    var name = input.value
+    console.log(name);
+   if(name === ""){
+       alert("Please write a name!"); 
+   } else {
+       alert("Thank you!")
+   }
 
+};
+
+function numberOfWordsChanged(event){
+    console.log(event.srcElement.value);
+}
+
+
+//new codegit 
+
+
+function test(event){
+  
+}
