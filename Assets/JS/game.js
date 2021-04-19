07859101abee2f1738599f64f8abf544898f75af
psurer <surereker@icloud.com>
2021-04-19 17:36:56 -0500

    added game html page & game js page

---
+++ Assets/JS/game.js
@@ -0,0 +1,73 @@
+//game logic var
+var translatedWords = []; //array to hold translated words
+var words = ["apple", "strawberry", "bread", "tea", "fish"]; //these words will come from dictionary API
+var correctAnswers = new Map(); // dictionary , key will be english source word, value will be correct translated word
+var selectedSourceWord = ''; // used to save the selected english word.
+var selectedTargetWord = ''; // used to save the target language word.
+
+//game logic 
+
+function playGame() {
+  for (var index = 0; index < words.length; index++) { // 
+    displayWordsToUser(words[index], "#wordsToTranslate"); // displays words 
+    translate(words[index]); // calls function translate to display the words
+  }
+}
+//calls google API to translate a word
+function translate(word) {
+  var url =
+    "https://translation.googleapis.com/language/translate/v2?key=AIzaSyCv96aME3EBXa609ZV3Pl8Z6rVgFVWmmAc";
+  url += "&source=EN";
+  url += "&target=FR";
+  url += "&q=" + word;
+  //gets data from google
+  $.get(url, function (returnByGoogle, status) {
+    console.log(returnByGoogle.data.translations[0].translatedText);
+    translatedWords.push(returnByGoogle.data.translations[0].translatedText);
+    //add to correct answers list
+    correctAnswers.set(
+      word,
+      returnByGoogle.data.translations[0].translatedText
+    );
+    displayWordsToUser(
+      returnByGoogle.data.translations[0].translatedText,
+      "#translatedWordsList"
+    );
+  });
+}
+//displays words to user
+function displayWordsToUser(word, elementID) {
+  if ('#translatedWordsList' === elementID) {
+    $(elementID).append(
+      `<li><input type="checkbox" disabled onclick="whenTranslatedWordIsClicked('${word}')"/>${word}</li>`
+    );
+  } else {
+    $(elementID).append(
+      `<li><input type="checkbox" onclick="whenSourceWordIsClicked('${word}')"/>${word}</li>`
+    );
+  }
+}
+//when user cliks on radio buttons function is called
+function whenSourceWordIsClicked(word) {
+  selectedSourceWord = word;
+  //enable target words list
+  $('#translatedWordsList').children('li').children('input').prop('disabled', false);
+}
+
+function whenTranslatedWordIsClicked(word) {
+  selectedTargetWord = word;
+  //now check the result
+  checkSelection();
+}
+
+function checkSelection(){
+  if ( correctAnswers.get(selectedSourceWord) === selectedTargetWord) {
+    alert("YOU ARE CORRECT!!!");
+  } else {
+    alert('NOPE!!!');
+  }
+}
+playGame();
+
+
+
--- Assets/JS/scripts.js
+++ Assets/JS/scripts.js
@@ -1,5 +1,5 @@
 var userLanguage;
-
+//this is a URL for the dictionary not a key 
 var spanishApiKey="https://dictionaryapi.com/api/v3/references/spanish/json/test?key=b5ae1ad4-6953-4b14-a6e0-f473b7f23015"
 //key, source, target, k , are parameters, you seperate by & 
 //example key=a877hjhj&target=FR& and so on
@@ -53,79 +53,9 @@ function checkFname(event) {
 
 };
 
-function numberOfWordsChanged(event){
+
+/* function numberOfWordsChanged(event){
     console.log(event.srcElement.value);
 }
-
-//game logic var
-var translatedWords = [];
-var words = ["apple", "strawberry", "bread", "tea", "fish"];
-var correctAnswers = new Map();
-var selectedSourceWord = ''; // Use to save the selected english word
-var selectedTargetWord = ''; // Use to save the target language word.
-
-//game logic
-function playGame() {
-  for (var index = 0; index < words.length; index++) {
-    displayWordsToUser(words[index], "#wordsToTranslate");
-    translate(words[index]);
-  }
-}
-//calls google API to translate a word
-function translate(word) {
-  var url =
-    "https://translation.googleapis.com/language/translate/v2?key=AIzaSyCv96aME3EBXa609ZV3Pl8Z6rVgFVWmmAc";
-  url += "&source=EN";
-  url += "&target=FR";
-  url += "&q=" + word;
-  //gets data from google
-  $.get(url, function (returnByGoogle, status) {
-    console.log(returnByGoogle.data.translations[0].translatedText);
-    translatedWords.push(returnByGoogle.data.translations[0].translatedText);
-    //add to correct answers list
-    correctAnswers.set(
-      word,
-      returnByGoogle.data.translations[0].translatedText
-    );
-    displayWordsToUser(
-      returnByGoogle.data.translations[0].translatedText,
-      "#translatedWordsList"
-    );
-  });
-}
-//displays words to user
-function displayWordsToUser(word, elementID) {
-  if ('#translatedWordsList' === elementID) {
-    $(elementID).append(
-      `<li><input type="checkbox" disabled onclick="whenTargetWordIsClicked('${word}')"/>${word}</li>`
-    );
-  } else {
-    $(elementID).append(
-      `<li><input type="checkbox" onclick="whenSourceWordIsClicked('${word}')"/>${word}</li>`
-    );
-  }
-}
-
-function whenSourceWordIsClicked(word) {
-  selectedSourceWord = word;
-  //enable target words list
-  $('#translatedWordsList').children('li').children('input').prop('disabled', false);
-}
-
-function whenTargetWordIsClicked(word) {
-  selectedTargetWord = word;
-  //now check the result
-  checkSelection();
-}
-
-function checkSelection(){
-  if ( correctAnswers.get(selectedSourceWord) === selectedTargetWord) {
-    alert("YOU ARE CORRECT!!!");
-  } else {
-    alert('NOPE!!!');
-  }
-}
-playGame();
-
-
+*/
 
