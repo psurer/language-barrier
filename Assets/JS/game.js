5c631abe6316e826f2fe963e9f8961c72cc45eb8
psurer <surereker@icloud.com>
2021-04-21 17:39:58 -0400

    updated step by step instructions to fix bug

--- Assets/JS/game.js
+++ Assets/JS/game.js
@@ -76,13 +76,13 @@ function checkSelection(){
 }
 // logic to clear check box on translated words section
 function clearUserSelectionsCheckBox() {
-  //Here you need to find the element with the id
-  //Then you need to find a way to find all the elements "children" that are LI
-  //once you get that you need to set a property named "checked" to false
+  //Find the element with the id
+  //Find a way to find all the elements "children" that are LI
+  //Then, set a property named "checked" to false
   //You do all that in one line of code using JQuery. 
-  //If you look at your code you will see an example of doing that to 
+  //If you look at our code in line 60 you will see an example of doing that to 
   //another element but basically the same concept. Looking for an element,
-  //finding children and setting a property
+  //finding children and setting a property.
     $('#translatedWordsList').children('li').children('input').prop('checked', false);
 }
 
