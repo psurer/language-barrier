12879592ac12a651850ee484fbb788927af01bb6
psurer <surereker@icloud.com>
2021-04-23 23:47:42 -0400

    fix img location;

--- Assets/JS/game.js
+++ Assets/JS/game.js
@@ -1,10 +1,10 @@
 //game logic var
 var translatedWords = []; //array to hold translated words
-var words = ["apple", "strawberry", "bread", "tea", "fish","avocado", "Chicken","grapes", "cucumber", "eggs", "spinach", "tomato", "beef","potatoes", "lemon", "lettuce", "pork","oranges", "mushrooms",]; //these words will come from dictionary API
+var words = ["apple", "strawberry", "bread", "tea", "fish", "avocado", "Chicken", "grapes", "cucumber", "eggs", "spinach", "tomato", "beef", "potatoes", "lemon", "lettuce", "pork", "oranges", "mushrooms",]; //these words will come from dictionary API
 var correctAnswers = new Map(); // dictionary , key will be english source word, value will be correct translated word
 var selectedSourceWord = ''; // used to save the selected english word.
 var selectedTargetWord = ''; // used to save the target language word.
-var selectedLanguage = '' ; /// use to save the language choosen by the user ( portuguese, spanish , frech)
+var selectedLanguage = ''; /// use to save the language choosen by the user ( portuguese, spanish , frech)
 var savedNamed = localStorage.getItem("name")
 console.log(savedNamed);
 var savedWordsQty = localStorage.getItem("words")
@@ -27,7 +27,7 @@ function translate(word) {
   var url =
     "https://translation.googleapis.com/language/translate/v2?key=AIzaSyCv96aME3EBXa609ZV3Pl8Z6rVgFVWmmAc";
   url += "&source=EN";
-  url += "&target="+ savedLanguage.toLocaleLowerCase();
+  url += "&target=" + savedLanguage.toLocaleLowerCase();
   url += "&q=" + word;
   //gets data from google
   $.get(url, function (returnByGoogle, status) {
@@ -72,33 +72,40 @@ function checkSelection() {
   if (correctAnswers.get(selectedSourceWord) === selectedTargetWord) {
     wins++;
     //I will scratch the words
-    $(`#${selectedTargetWord}`).addClass('scratched').children('input').prop('disabled',true).prop('checked', false);;
-    $(`#${selectedSourceWord}`).addClass('scratched').children('input').prop('disabled',true).prop('checked', false);
-   //Adding the image of the selected word
-   correctWord=$(`#${selectedSourceWord}`).selector
-   //removing the # from the selector
-   var imageWord = correctWord.replace('#','');
-   console.log(imageWord);
-   getImage(imageWord);
-   
+    $(`#${selectedTargetWord}`).addClass('scratched').children('input').prop('disabled', true).prop('checked', false);;
+    $(`#${selectedSourceWord}`).addClass('scratched').children('input').prop('disabled', true).prop('checked', false);
+    //Adding the image of the selected word
+    correctWord = $(`#${selectedSourceWord}`).selector
+    //removing the # from the selector
+    var imageWord = correctWord.replace('#', '');
+    console.log(imageWord);
+    getImage(imageWord);
+
   } else {
     losses++;
     clearUserSelectionsCheckBox();
   }
   showUserScore();
-  const maxNumberOfWords = localStorage.getItem("words");
-  if (wins == maxNumberOfWords) {
-     window.location.href="congrats.html";
+  const maxNumberOfWords = parseInt(localStorage.getItem("words"));
+  if (wins === maxNumberOfWords) {
+    saveScoreToLocalStorage();
+    window.location.href = "congrats.html";
   }
+}
+
+function saveScoreToLocalStorage() {
+  //saves score to local storage
+  wins = wins + parseInt(localStorage.getItem('wins')); // Before saving we add the current score to the saved one. Previous game.
+  localStorage.setItem("wins", wins);
+  losses = losses + parseInt(localStorage.getItem('losses')); // Before saving we add the current score to the saved one. Previous game.
+  localStorage.setItem("losses", losses);
 }
 
 function showUserScore() {
   $("#right").html(`<h3> wins ${wins} </h3>`);
   $("#wrong").html(`<h3> losses ${losses} </h3>`);
-  //saves score to local storage
-  localStorage.setItem("wins",wins);
-  localStorage.setItem("losses",losses);
 }
+
 // logic to clear check box on translated words section
 function clearUserSelectionsCheckBox() {
   //Find the element with the id
@@ -111,50 +118,42 @@ function clearUserSelectionsCheckBox() {
   $('#translatedWordsList').children('li').children('input').prop('checked', false);
 }
 
-function initGameBoard(){
+function initGameBoard() {
   $('#translatedWordsList').html(null);
   $('#wordsToTranslate').html(null);
 }
 
 playGame();
 
-//getImage();
-
-//Listening the user selection
-//$(`#${selectedTargetWord}`).on("click", function(){
-  //correctWord=$(`#${selectedTargetWord}`)
-  //console.log("test")
-  //getImage(correctWord);
-
 //Image API link
- //Function to get and display the picture 
-  function getImage(imageWord) {
+//Function to get and display the picture 
+function getImage(imageWord) {
   const myHeaders = new Headers();
   myHeaders.append('content-type', 'application/json'); // Adding content type to myHeaders
-  myHeaders.append('Authorization','563492ad6f91700001000001067ba0f78afa4701a9963ea68164e74c'); // Adding the API KEY
-  const imageUrl = "https://api.pexels.com/v1/search?query="+imageWord+"&per_page=1";
+  myHeaders.append('Authorization', '563492ad6f91700001000001067ba0f78afa4701a9963ea68164e74c'); // Adding the API KEY
+  const imageUrl = "https://api.pexels.com/v1/search?query=" + imageWord + "&per_page=1";
   console.log(imageUrl)
   fetch(imageUrl, {
-  mode: 'cors', // Adding the fetch mode to use cors
-  method: "GET",
-  headers: myHeaders // Adding the fetch call headers.
-     }).then(function (response) {
-  if (response.ok) {
-  console.log(response);
-  response.json().then(function (data) {
-  console.log(data);
-  //Showing the picture
-  $('#image').attr('src', data.photos[0].src.medium); // here I am adding the photo url to the image sample tag
-  
-  })
-  }
-  else {
-  console.log(response);
-   }}).catch(
-  function (error, status) {
-  console.log(error);
-  console.log(status);
-      });
+    mode: 'cors', // Adding the fetch mode to use cors
+    method: "GET",
+    headers: myHeaders // Adding the fetch call headers.
+  }).then(function (response) {
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function (data) {
+        console.log(data);
+        //Showing the picture
+        $('#gameBoard').attr('src', data.photos[0].src.small ); // here I am adding the photo url to the image sample tag
+      })
     }
+    else {
+      console.log(response);
+    }
+  }).catch(
+    function (error, status) {
+      console.log(error);
+      console.log(status);
+    });
+}
 
 
